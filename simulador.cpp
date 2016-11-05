2b34a738753dc80aeb3c3ebbf7e0da1b30fc21a7
AugustC <augusto.cms7@gmail.com>
2016-11-05 16:46:35 -0200

    criaArquivo* implementado

--- simulador.cpp
+++ simulador.cpp
@@ -1,3 +1,4 @@
+#include "Processos.hpp"
 #include<iostream>
 
 // Gerencia de espaco livre
@@ -16,7 +17,22 @@ void LRU();
 void zeraR();
 void copiaMem_Vir();
 void copiaVir_Mem();
-FILE * criaArquivoMem();
-FILE * criaArquivoVir();
+fstream criaArquivoMem(total){
+    // Cria o arquivo /tmp/ep3.mem e preenche com -1
+    fstream arquivo_mem;
+    int aux = -1;
+    arquivo_mem.open("/tmp/ep3.mem", ios::binary | ios::out | ios::in);
+    arquivo_mem.write(&aux, total); // Se total for em bytes
+    return arquivo_mem;
+}
+
+fstream criaArquivoVir(virtual_m){
+    // Cria o arquivo /tmp/ep3.mem e preenche com -1
+    fstream arquivo_vir;
+    int aux = -1;
+    arquivo_vir.open("/tmp/ep3.vir", ios::binary | ios::out | ios::in);
+    arquivo_vir.write(&aux, virtual_m);
+    return arquivo_vir;
+}
 
 void simulador(FILE * arquivo, int gerenciadorMemoria, int paginacao, int intervalo);
