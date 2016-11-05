71e2156096c1f938a18801baa7a269ad63c8ba19
Lucas Helfstein Rocha dos Santos <lucashelfs@scheme.linux.ime.usp.br>
2016-11-05 17:11:35 -0200

    Processos tomando forma...

--- Processos.hpp
+++ Processos.hpp
@@ -1,13 +1,25 @@
 #include<iostream>
 
-class Processos {
-    int base;
-    int limite;
-    unsigned int PID;
-    int* p;
-    int* t;
-    int indice;
-
-    // Implementar comparacao entre Processos, i. e.,
-    // Processo A > B, <=> A.t[ind] > B.t[ind]
+class Processo {
+	    int base;
+	    int limite;
+	    unsigned int PID;
+	    int* p;
+	    int* t;
+	    int indice;
+	public:
+		int pega_endereco() return p[indice] + base;
 }
+
+
+// Implementar comparacao entre Processos, i. e.,
+// Processo A > B, <=> A.t[ind] > B.t[ind]
+
+bool compara (const Processo & a, const Processo & b) 
+{
+	return ( a.indice < b.indice);
+}  
+
+// #include <list>
+// std::list<Processo> lista;
+// lista.sort(compara);  // aproximadamente N logN
