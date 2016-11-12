2b1882b05dadc3fe9823fb48de4606d612bbaa8b
AugustC <augusto.cms7@gmail.com>
2016-11-12 11:08:13 -0200

    getPID implementado

--- Processos.hpp
+++ Processos.hpp
@@ -6,16 +6,17 @@ class Processo {
     int base;
     std::list<int> p;
     std::list<int> t;
-
+    unsigned int PID;
+    
 public:
 
-    unsigned int PID;
     std::string nome;
     int limite;
     
     Processo(int limite, int PID, std::list<int> p, std::list<int> t, std::string nome);
     void definir_base(int base);
     int pega_endereco();
+    std::string getPID();
     int proximo_tempo() const;
     bool operator<(Processo b) const;
 };
