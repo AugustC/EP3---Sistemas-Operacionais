bf3edad8aff7865d5a8bb36b2362ff96b2bf6bdd
lucashelfstein <lucashelfstein@gmail.com>
2016-11-18 18:53:07 -0200

    Gerador 2.1

--- gen_trace.py
+++ gen_trace.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 # Numero de processos que serao gerados
-qtd = 100000
+qtd = 10000	
 
 # Tamanho da memoria virtual
 mem_vir = 10000
@@ -35,24 +35,44 @@ mem_ocup = np.zeros(total_exec)
 # Tempos de para os traces e acessos
 for i in range (1, qtd + 1): 
 
-	# tempo de saida
-	x[i][1] = random.randint (x[i][0], x[i][0]+15) 
+	# tempo de saida eh o maximo 
+	x[i][1] = random.randint (x[i][0], total_exec) 
 
 	# tamanho do processo
 	x[i][2] = random.randint (5, 30) 
 
 	# escolhe 5 tempos e ordena
 	for j in range (0, 5):
-		y[j] = random.randint (x[i][0], x[i][1])
+		y[j] = random.randint(x[i][0], x[i][1])
 	y.sort();
 
 	# processo vai acessar em um tempo, se naquele momento tiver memoria disponivel
 	for j in range (0, 5):
 		
-		if mem_ocup[y[j]] + tam_pagina < mem_fis:
+		tf = 0
+
+		while y[j] < total_exec and not (mem_ocup[y[j]] + tam_pagina < mem_fis):
+			y[j] = y[j] + 1
+
+		# se foi no tempo permitido
+		if y[j] < total_exec:
+
+			# atualiza o tf, se necessario
+			if y[j] > tf:
+				tf = y[j]
+				x[i][1] = tf
+
+			# pi, ti
 			x[i][2*j+3] = random.randint (0, x[i][2])
 			x[i][2*j+4] = y[j]
 			mem_ocup[y[j]] = mem_ocup[y[j]] + tam_pagina
+
+		# se cai muito nesse caso, considere aumentar seus parametros...
+		else:
+			break
+
+	# pode ordenar aqui de novo pq temos garantia de que cabe na memoria
+	y.sort();
 
 # header
 x[0][0] = mem_fis                # total
@@ -62,7 +82,7 @@ x[0][3] = tam_pagina             # p
 
 np.savetxt ('trace_inicial', x, delimiter = ' ', fmt='%d')
 
-# nomeia os processos
+# nomeia os processos, etc
 cont = 0
 with open('trace_inicial', 'r') as f:
 	
